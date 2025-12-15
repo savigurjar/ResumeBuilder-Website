22d930a1abcb0661a150f40cfb938d93dcb63d4b
savigurjar <savigurjar1201@gmail.com>
2025-12-15 16:00:52 +0530

    navbar

--- vite-project/src/pages/Layout.jsx
+++ vite-project/src/pages/Layout.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import { Outlet } from 'react-router'
+import Navbar from '../components/Navbar'
 
 function Layout() {
   return (
     <div>
-        <h1>Layout page</h1>
-        <Outlet/>
+        
+        <div className='min-h-screen bg-gray-50' >
+          <Navbar/>
+          <Outlet/>
+        </div>
     </div>
   )
 }
